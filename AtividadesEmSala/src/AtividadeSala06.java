4748632f8b1fa418bf42013487fa6a82dbdcd7a9
futebol0254 <carlos93457523@gmail.com>
2025-08-25 18:54:21 -0300

    Resolução das Atividades

--- AtividadesEmSala/src/AtividadeSala06.java
+++ AtividadesEmSala/src/AtividadeSala06.java
@@ -1,21 +1,24 @@
 public class AtividadeSala06 {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
+
         int a = 5, b = -5, x = 30, y = 9;
         boolean teste, verdadeiro = true, falso = false;
-        teste = (a > b) || (a < b);// true
+        teste = (a > b) || (a < b);
         System.out.println("(a > b) || (a < b): " + teste);
-        teste = (x > a) && (y > a);// true
-        System.out.println("(x > a) && (y > a)" + teste);
+        teste = (x > a) && (y > a);
+        System.out.println("(x > a) && (y > a): " + teste);
         teste = !verdadeiro;
-        System.out.println("Negação de um valor verdadeiro" + teste);
+        System.out.println("Negação de um valor verdadeiro: "
+                +teste);
 
         teste = verdadeiro || falso;
         System.out.println("verdadeiro || falso = " + teste);
         teste = !(falso || falso) && verdadeiro;
-        System.out.println("! (falso || falso) && verdadeiro = " + teste);
+        System.out.println("!(falso || falso) && verdadeiro = " + teste);
         teste = verdadeiro || falso && verdadeiro;
         System.out.println("verdadeiro || falso && verdadeiro = " + teste);
         teste = verdadeiro && !(falso && falso);
-        System.out.println("verdadeiro && !(falso && falso)= " + teste);
+        System.out.println("verdadeiro && !(falso && falso)" + teste);
     }
 }
