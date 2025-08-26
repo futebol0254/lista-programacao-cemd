174ba9ca4c229d4a8ba4df82c4fc1250631eed4b
futebol0254 <carlos93457523@gmail.com>
2025-08-26 18:57:25 -0300

    Atividade em Sala de Aula

---
+++ AtividadesEmSala/src/AtividadeSala14.java
@@ -0,0 +1,15 @@
+public class AtividadeSala14 {
+
+    public static int soma(int x, int y){
+        return x + y;
+    }
+
+    public static void imprimir(int valor) {
+        System.out.println(valor);
+    }
+
+    public static void main(String[] args) {
+
+        imprimir(soma(20, 35));
+    }
+}
