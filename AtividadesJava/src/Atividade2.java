6bc0338ce7626e0097abb954fb7ea5780cc41912
futebol0254 <carlos93457523@gmail.com>
2025-08-28 19:47:49 -0300

    Resolução da Atividade 2

---
+++ AtividadesJava/src/Atividade2.java
@@ -0,0 +1,13 @@
+public class Atividade2 {
+
+    public static void main(String[] args) {
+        String dia = "17";
+        System.out.println("Dia " + dia);
+
+        String mês = "Setembro";
+        System.out.println("Mês de " + mês);
+
+        String ano = "2005";
+        System.out.println("Ano " + ano);
+    }
+}
