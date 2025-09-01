cacef5f7d67e370e3b7132bb915de9c0b8e5b973
futebol0254 <carlos93457523@gmail.com>
2025-09-01 18:45:21 -0300

    Resolução da Atividade 7

---
+++ AtividadesJava/src/Atividade7.java
@@ -0,0 +1,13 @@
+public class Atividade7 {
+
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println("Digite o primeiro número (a): ");
+
+        int b = 8;
+        System.out.println("Digite o segundo número (b): ");
+
+        int soma = a + b;
+        System.out.println("A soma é: " + soma);
+    }
+}
