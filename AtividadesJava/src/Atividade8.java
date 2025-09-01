e9ca55bcf7cbc48d14cc9a844cce6f2ac4b875c4
futebol0254 <carlos93457523@gmail.com>
2025-09-01 19:14:45 -0300

    Resolução da Atividade 8

--- AtividadesJava/src/Atividade8.java
+++ AtividadesJava/src/Atividade8.java
@@ -1,19 +1,16 @@
 import java.util.Scanner;
-
 public class Atividade8 {
     public static void main(String[] args) {
         Scanner leia = new Scanner(System.in);
-
+        double n1 = 0, n2 = 0, n3 = 0, media = 0;
         System.out.println("Digite a primeira nota: ");
-        double n1 = leia.nextDouble();
-
+        n1 = leia.nextDouble();
         System.out.println("Digite a segunda nota: ");
-        double n2 = leia.nextDouble();
-
+        n2 = leia.nextDouble();
         System.out.println("Digite a terceira nota: ");
-        double n3 = leia.nextDouble();
+        n3 = leia.nextDouble();
+        media = (n1 + n2 + n3)/3;
+        System.out.println("A média das notas é: " + Math.round(media));
 
-        double media = (n1 + n2 + n3) /3;
-        System.out.println("A média é: , media ");
     }
 }
